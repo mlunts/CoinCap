c987aa9dbd28ef37c90ab94fdc16f274937a1b28
Marina Lunts <marinalunts@gmail.com>
2025-01-04 15:18:05 +0100

    Initial Commit

---
+++ CoinCap/CoinCapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinCapApp.swift
+//  CoinCap
+//
+//  Created by Marina Lunts on 04.01.25.
+//
+
+import SwiftUI
+
+@main
+struct CoinCapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinCapTests/CoinCapTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoinCapTests.swift
+//  CoinCapTests
+//
+//  Created by Marina Lunts on 04.01.25.
+//
+
+import Testing
+@testable import CoinCap
+
+struct CoinCapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
