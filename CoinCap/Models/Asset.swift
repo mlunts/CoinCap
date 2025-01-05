8ae9a493926f2b11a9c5cfee58b414f2d584c0ef
Marina Lunts <marinalunts@gmail.com>
2025-01-05 21:49:14 +0100

    Created calculated value isPositivePercentageChange; formatted Asset structure

--- CoinCap/Models/Asset.swift
+++ CoinCap/Models/Asset.swift
@@ -10,17 +10,35 @@ struct Asset: Identifiable, Codable, Equatable {
     let id: String
     let symbol: String
     let name: String
-    let supply: String?
-    let marketCapUsd: String?
-    let volumeUsd24Hr: String?
-    let priceUsd: String?
-    let changePercent24Hr: String?
+    private let supply: String?
+    private let marketCapUsd: String?
+    private let volumeUsd24Hr: String?
+    private let priceUsd: String?
+    private let changePercent24Hr: String?
     
+    init(id: String, symbol: String, name: String, supply: String?, marketCapUsd: String?, volumeUsd24Hr: String?, priceUsd: String?, changePercent24Hr: String?) {
+        self.id = id
+        self.symbol = symbol
+        self.name = name
+        self.supply = supply
+        self.marketCapUsd = marketCapUsd
+        self.volumeUsd24Hr = volumeUsd24Hr
+        self.priceUsd = priceUsd
+        self.changePercent24Hr = changePercent24Hr
+    }
+}
+
+// MARK: Calculated values
+extension Asset {
     var percentageChange: String {
         if let change = changePercent24Hr, let value = Double(change) {
             return String(format: "%.2f%%", value)
         }
         return "0.00%"
+    }
+    
+    var isPositivePercentageChange: Bool {
+        percentageChange.hasPrefix("+")
     }
     
     var price: String {
