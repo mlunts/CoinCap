2feabd37f05ec524a9ce4a1bc61ec5fa3867c5e2
Marina Lunts <marinalunts@gmail.com>
2025-01-05 21:15:53 +0100

    Removed unused files

--- CoinCap/Models/Asset.swift
+++ CoinCap/Models/Asset.swift
@@ -6,12 +6,35 @@
 //
 
 import Foundation
-
-struct Asset: Identifiable {
+struct Asset: Identifiable, Codable, Equatable {
     let id: String
-    let iconURL: URL?
-    let name: String
     let symbol: String
-    let price: String
-    let percentageChange: String
+    let name: String
+    let supply: String?
+    let marketCapUsd: String?
+    let volumeUsd24Hr: String?
+    let priceUsd: String?
+    let changePercent24Hr: String?
+    
+    var percentageChange: String {
+           if let change = changePercent24Hr, let value = Double(change) {
+               return String(format: "%.2f%%", value)
+           }
+           return "0.00%"
+       }
+       
+       var price: String {
+           if let price = priceUsd, let value = Double(price) {
+               return "$" + String(format: "%.2f", value)
+           }
+           return "$0.00"
+       }
+    
+    var iconURL: URL? {
+        URL(string: "https://assets.coincap.io/assets/icons/\(symbol.lowercased())@2x.png")
+    }
+}
+
+struct AssetResponse: Codable {
+    let data: [Asset]
 }
