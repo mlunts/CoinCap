9d93d60949b6b7377377c3d138746fe49c0bc556
Marina Lunts <marinalunts@gmail.com>
2025-01-06 15:43:26 +0100

    Added colors to assets

---
+++ CoinCap/Views/Components/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  CoinCap
+//
+//  Created by Marina Lunts on 06.01.25.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorView()
+}
